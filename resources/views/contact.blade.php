6364fe8697fb446746c6dbf19b3f74f3a9becd27
indicartans <indica464@gmail.com>
2024-09-21 23:11:39 +0700

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h3 class="text-xl">Contacts</h3>
+    <ul>
+        <li>
+            <a href="https://instagram.com/alfaindicadz">Instagram</a>
+        </li>
+        <li>
+            <a href="https://github.com/indicartans">Github</a>
+        </li>
+    </ul>
+</x-layout>
