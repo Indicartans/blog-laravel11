18af5e6cec31b35502cbe5a4fe06f5212caca66a
indicartans <indica464@gmail.com>
2024-09-24 12:07:15 +0700

    make model

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -11,58 +12,17 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About']);
 });
 
+
 Route::get('/posts', function () {
     return view('posts', [
         'title' => 'Blog',
-        'posts' => [
-            [
-                'id' => '1',
-                'title' => 'Judul Artikel 1',
-                'slug' => 'judul-artikel-1',
-                'author' => 'Alfa Indica',
-                'body' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, dolores iste
-                        molestias sint rerum, sapiente dolor ducimus non minus, quam corporis sequi quasi velit. Nesciunt
-                        maxime perferendis aut hic veniam?"
-            ],
-            [
-                'id' => '2',
-                'title' => 'Judul Artikel 2',
-                'slug' => 'judul-artikel-2',
-                'author' => 'Alfa Indica',
-                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum
-                    consequuntur iste ullam fugit delectus fugiat! Tenetur doloribus alias consequuntur iusto, nemo non
-                    velit ad aliquam repellendus dolorem quas quos suscipit.'
-            ]
-        ]
-
+        'posts' => Post::all()
     ]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => '1',
-            'title' => 'Judul Artikel 1',
-            'slug' => 'judul-artikel-1',
-            'author' => 'Alfa Indica',
-            'body' => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, dolores iste
-                        molestias sint rerum, sapiente dolor ducimus non minus, quam corporis sequi quasi velit. Nesciunt
-                        maxime perferendis aut hic veniam?"
-        ],
-        [
-            'id' => '2',
-            'title' => 'Judul Artikel 2',
-            'slug' => 'judul-artikel-2',
-            'author' => 'Alfa Indica',
-            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum
-                    consequuntur iste ullam fugit delectus fugiat! Tenetur doloribus alias consequuntur iusto, nemo non
-                    velit ad aliquam repellendus dolorem quas quos suscipit.'
-        ]
-    ];
 
-    $post = Arr::first($posts, function ($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
+    $post = Post::find($slug);
     // dd($post);
 
     return view('post', ['title' => 'Single Post', 'post' => $post]);
