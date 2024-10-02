8c7b4fd5de45c5f954490ed1711bafc5a8b71d1e
indicartans <indica464@gmail.com>
2024-10-02 09:53:00 +0700

    add category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,8 +3,12 @@
     {{-- @dd($posts) --}}
     <article class="py-8 max-w-screen-md ">
         <h2 class="text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
-        <div class="text-base text-gray-500">
-            <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> |
+        <div class="text-base">
+            By
+            <a href="/authors/{{ $post->author->username }}"
+                class="hover:underline  text-gray-500">{{ $post->author->name }}</a>
+            In <a href="/categories/{{ $post->category->slug }}"
+                class="hover:underline  text-gray-500">{{ $post->category->name }}</a> |
             {{ $post->updated_at->format('d M Y') }}
             <p class="my-4 font-light">
                 {{ $post['body'] }}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,8 +6,12 @@
             <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
                 <h2 class="text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
             </a>
-            <div class="text-base text-gray-500">
-                <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> |
+            <div class="text-base">
+                By
+                <a href="/authors/{{ $post->author->username }}"
+                    class="hover:underline  text-blue-600">{{ $post->author->name }}</a>
+                in <a href="/categories/{{ $post->category->slug }}"
+                    class="hover:underline  text-blue-600">{{ $post->category->name }}</a> |
                 {{ $post->updated_at->format('d M Y') }}
                 <p class="my-4 font-light">
                     {{ Str::limit($post['body'], 100) }}
